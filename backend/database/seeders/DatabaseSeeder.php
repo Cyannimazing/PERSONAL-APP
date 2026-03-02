7cb6ee9b0e3f33ffb6cdcabe754a0054a27a35e1
Cyril Narvasa <cyrilnarvasa589@gmail.com>
2026-03-02 19:39:13 +0800

    FIX SEEDER ERROR

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            ApplicationSeeder::class,
+            HelloWorldSeeder::class,
         ]);
     }
 
--- backend/database/seeders/HelloWorldSeeder.php
+++ backend/database/seeders/HelloWorldSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Application;
 
-class ApplicationSeeder extends Seeder
+class HelloWorldSeeder extends Seeder
 {
     public function run(): void
     {
